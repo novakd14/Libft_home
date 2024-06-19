7033c56e4210864303d2a45c13892e97070d9d0c
David Novak <dnovak@student.42prague.com>
2024-06-19 18:23:42 +0200

    Starting up unity testing for libft.

--- unity_test/libft_test.h
+++ unity_test/libft_test.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf_test.h                                 :+:      :+:    :+:   */
+/*   libft_test.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 16:44:22 by dnovak            #+#    #+#             */
-/*   Updated: 2024/06/09 14:35:59 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/06/19 18:09:13 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_TEST_H
 # define LIBFTPRINTF_TEST_H
-# include "../libftprintf.h"
+# include "../libft.h"
 # include "unity.h"
 # include <fcntl.h>  // flags for open
 # include <stdio.h>  // printf, fflush
