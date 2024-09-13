864dd75a81b7002fca4e7b14539bbe2ada7ec291
dnovak_home <dnovak@student.42.fr>
2024-09-13 14:14:29 +0200

    Added ft_printf in the libft. Plus started unit testing functions.

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,25 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_test.h                                       :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/24 16:44:22 by dnovak            #+#    #+#             */
-/*   Updated: 2024/09/13 14:11:24 by dnovak           ###   ########.fr       */
+/*   Created: 2024/06/19 15:03:02 by dnovak            #+#    #+#             */
+/*   Updated: 2024/09/10 00:02:58 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_TEST_H
-# define LIBFTPRINTF_TEST_H
-# include "../libft.h"
-# include "unity.h"
-# include <fcntl.h>  // flags for open
-# include <stdio.h>  // printf, fflush
-# include <unistd.h> // open, close, dup, dup2
-# define MAX_INT 2147483647
-# define MIN_INT -2147483648
-# define MAX_UINT 4294967295
-# define MIN_UINT 0
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "libftprintf.h"
 
-#endif // LIBFTPRINTF_TEST_H
+#endif // FT_PRINTF_H
