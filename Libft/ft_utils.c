3916b9215505674dc5d7c291be1d10d4e7c8cfd8
dnovak_home <dnovak@student.42.fr>
2024-10-21 15:11:50 +0200

    Better implementation of ft_printf in Libft.

---
+++ Libft/ft_utils.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 12:34:55 by dnovak            #+#    #+#             */
+/*   Updated: 2024/10/21 12:35:43 by dnovak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned long	ft_umin(unsigned long a, unsigned long b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+unsigned long	ft_umax(unsigned long a, unsigned long b)
+{
+	if (a < b)
+		return (b);
+	return (a);
+}
+
+long	ft_min(long a, long b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+long	ft_max(long a, long b)
+{
+	if (a < b)
+		return (b);
+	return (a);
+}
