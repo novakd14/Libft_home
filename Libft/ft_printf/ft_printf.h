111d9adb9b303f8b2fb0c63759d7690f09f8b83b
dnovak_home <dnovak@student.42.fr>
2024-10-21 15:11:50 +0200

    Better implementation of ft_printf in Libft.

--- Libft/ft_printf/ft_printf.h
+++ Libft/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:03:02 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/21 11:54:35 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/21 12:37:25 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,39 @@
 
 typedef struct s_flags
 {
-	char		alter_form;
-	char		zero_padd;
-	char		left_adjust;
-	char		space_signed;
-	char		sign_signed;
-	int			field_width;
-	int			precision;
-	char		*str;
-	size_t		arg_len;
-}				t_flags;
+	char	alter_form;
+	char	zero_padd;
+	char	left_adjust;
+	char	space_signed;
+	char	sign_signed;
+	int		field_width;
+	int		precision;
+	char	*str;
+	size_t	arg_len;
+}			t_flags;
 
-int				ft_printf(const char *format, ...);
-int				ft_set_flags(const char *format, size_t *i, t_flags *flags,
-					va_list *argv);
+int			ft_set_flags(const char *format, size_t *i, t_flags *flags,
+				va_list *argv);
 
 // Functions for saving argument to string
-int				ft_save_char(t_flags *flags, unsigned char c);
-int				ft_save_str(t_flags *flags, char *c);
-int				ft_save_ptr(t_flags *flags, void *ptr);
-int				ft_save_int(t_flags *flags, int n);
-int				ft_save_uint(t_flags *flags, unsigned int n);
-int				ft_save_hexa_small(t_flags *flags, unsigned int x);
-int				ft_save_hexa_big(t_flags *flags, unsigned int x);
-int				ft_save_pct(t_flags *flags);
-int				ft_save_flags(t_flags *flags);
-void			ft_cpyhexadec_small(char *dest, int len, unsigned long x);
-void			ft_cpyhexadec_big(char *dest, int len, unsigned long x);
-void			ft_cpynbr(char *dest, int last, int n);
+int			ft_save_char(t_flags *flags, unsigned char c);
+int			ft_save_str(t_flags *flags, char *c);
+int			ft_save_ptr(t_flags *flags, void *ptr);
+int			ft_save_int(t_flags *flags, int n);
+int			ft_save_uint(t_flags *flags, unsigned int n);
+int			ft_save_hexa_small(t_flags *flags, unsigned int x);
+int			ft_save_hexa_big(t_flags *flags, unsigned int x);
+int			ft_save_pct(t_flags *flags);
+int			ft_save_flags(t_flags *flags);
+void		ft_cpyhexadec_small(char *dest, int len, unsigned long x);
+void		ft_cpyhexadec_big(char *dest, int len, unsigned long x);
+void		ft_cpynbr(char *dest, int last, int n);
 
 // Print argument
-int				ft_put_arg(t_flags *flags);
+int			ft_put_arg(t_flags *flags);
 
-// Useful functions
-int				ft_checkhexadec(unsigned long int x);
-int				ft_checkunbr(unsigned int u);
-unsigned long	ft_umin(unsigned long a, unsigned long b);
-unsigned long	ft_umax(unsigned long a, unsigned long b);
-long			ft_min(long a, long b);
-long			ft_max(long a, long b);
+// Check functions
+int			ft_checkhexadec(unsigned long int x);
+int			ft_checkunbr(unsigned int u);
 
 #endif // FT_PRINTF_H
